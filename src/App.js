eddb2c786b8b7412b5fb78d7f7652fe92fb9e69b
QIAN FANG <you@example.com>
2021-09-21 11:37:03 -0600

    init

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import {Container} from './components/styles/Container.styled'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Card from './components/Card'
+import {ThemeProvider } from 'styled-components'
+import GlobalStyles from './components/styles/Global'
+import content from './content'
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333'
+  },
+
+  mobile: '768px',
+}
+
+function App() {
+  return (
+    <ThemeProvider theme = {theme}>
+      <GlobalStyles/>
+      <Header />
+      <Container>
+        {content.map((item,idx)=>(
+          <Card key={idx} item={item} />
+        ))}
+      </Container>
+      <Footer/>
+    </ThemeProvider>
+  );
+}
+
+export default App;
